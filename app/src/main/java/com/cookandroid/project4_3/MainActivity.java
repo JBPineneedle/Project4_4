bd3843091c1c63a67132981794c6e4025516f1b0
JBPineneedle <JBGPineneedle@gmail.com>
2023-04-10 20:34:16 +0900

    upload

--- app/src/main/java/com/cookandroid/project4_3/MainActivity.java
+++ app/src/main/java/com/cookandroid/project4_3/MainActivity.java
@@ -11,70 +11,90 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView text1, text2;
-    CheckBox chkAgree;
-    RadioGroup rGroup1;
-    RadioButton rdoDog, rdoCat, rdoRabbit;
-    Button btnOK;
-    ImageView imgPet;
 
+    Switch swAgree;
+    TextView tvText1, tvText2;
+    RadioGroup rgGroup1;
+    RadioButton rQ,rR,rS;
+    Button btnFinish, btnReset;
+    ImageView imgVer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("애완동물 사진 보기");
+        setTitle("안드로이드 사진 보기");
+        swAgree = findViewById(R.id.swAgree);
+        tvText1 = findViewById(R.id.text1);
+        tvText2 = findViewById(R.id.text2);
+        rgGroup1 = findViewById(R.id.rGroup1);
+        rQ = findViewById(R.id.RQ);
+        rR = findViewById(R.id.RR);
+        rS = findViewById(R.id.RS);
+        btnFinish = findViewById(R.id.btnExit);
+        btnReset = findViewById(R.id.btnReset);
+        imgVer = findViewById(R.id.ImgVer);
 
-        text1 = (TextView) findViewById(R.id.Text1);
-        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
-
-        text2 = (TextView) findViewById(R.id.Text2);
-        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
-        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
-        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
-        rdoRabbit=(RadioButton) findViewById(R.id.RdoRabbit);
-
-        btnOK = (Button) findViewById(R.id.BtnOK);
-        imgPet = (ImageView) findViewById(R.id.ImgPet);
-
-        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        // swAgree 스위치 클릭 리스너 설정
+        swAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (chkAgree.isChecked() == true) {
-                    text2.setVisibility(android.view.View.VISIBLE);
-                    rGroup1.setVisibility(android.view.View.VISIBLE);
-                    btnOK.setVisibility(android.view.View.VISIBLE);
-                    imgPet.setVisibility(android.view.View.VISIBLE);
-                } else {
-                    text2.setVisibility(android.view.View.INVISIBLE);
-                    rGroup1.setVisibility(android.view.View.INVISIBLE);
-                    btnOK.setVisibility(android.view.View.INVISIBLE);
-                    imgPet.setVisibility(android.view.View.INVISIBLE);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // 스위치가 ON 상태일 때
+                if (isChecked) {
+                    tvText2.setVisibility(View.VISIBLE);
+                    rgGroup1.setVisibility(View.VISIBLE);
+                    btnFinish.setVisibility(View.VISIBLE);
+                }
+                // 스위치가 OFF 상태일 때
+                else {
+                    tvText2.setVisibility(View.INVISIBLE);
+                    rgGroup1.setVisibility(View.INVISIBLE);
+                    btnFinish.setVisibility(View.INVISIBLE);
+                    imgVer.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
-        btnOK.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0){
-                switch (rGroup1.getCheckedRadioButtonId()){
-                    case R.id.RdoDog:
-                        imgPet.setImageResource(R.drawable.dog);
-                        break;
-                    case R.id.RdoCat:
-                        imgPet.setImageResource(R.drawable.cat);
-                        break;
-                    case R.id.RdoRabbit:
-                        imgPet.setImageResource(R.drawable.rabbit);
-                        break;
-                    default:
-                        Toast.makeText(getApplicationContext(),"동물을 먼저 선택하세요.",Toast.LENGTH_SHORT).show();
+        // 라디오버튼 클릭 리스너 설정
+        rgGroup1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.RQ) {
+                    imgVer.setImageResource(R.drawable.q);
+                    imgVer.setVisibility(View.VISIBLE);
+                }
+                else if (checkedId == R.id.RR) {
+                    imgVer.setImageResource(R.drawable.r);
+                    imgVer.setVisibility(View.VISIBLE);
+                }
+                else if (checkedId == R.id.RS) {
+                    imgVer.setImageResource(R.drawable.s);
+                    imgVer.setVisibility(View.VISIBLE);
                 }
             }
-    });
+        });
 
+        // 종료버튼 클릭 리스너 설정
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                btnFinish.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // 초기화버튼 클릭 리스너 설정
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swAgree.setChecked(false);
+                btnReset.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
